bee8a57e5e16694fd243664c250870f5af5cd923
zhijie.xu <zhijie.xu@openjawtech.com>
2021-03-29 22:52:58 +0800

    Remove unused url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,3 @@
 global.constants = {
-    serverUrl:'http://120.26.45.1:8084/cityWeather/',
-    jsonDataUrl: 'http://5c92dbfae7b1a00014078e61.mockapi.io/owners'
+    serverUrl:'http://120.26.45.1:8084/cityWeather/'
 };
