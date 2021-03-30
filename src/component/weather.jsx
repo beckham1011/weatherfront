685065a0f4c37536508f962ae6c55920faecd8cd
zhijie.xu <zhijie.xu@openjawtech.com>
2021-03-30 15:28:12 +0800

    pets function

--- src/component/weather.jsx
+++ src/component/weather.jsx
@@ -6,42 +6,43 @@ import '../config'
 class Weather extends React.Component {
     constructor(props) {
         super(props);
-    
         let url = global.constants.serverUrl
         axios.get(url).then(
           (response) => {
-            var petsMap = {}
-            for(var i =0; i<response.data.length; i++){
-              var element = response.data[i]
-              if(petsMap.hasOwnProperty(element.gender)){
-                petsMap[element.gender].push(element.name)
-                petsMap[element.gender] = pets
-              }else{
-                var pets = new Array()
-                pets.push(element.name)
-                petsMap[element.gender] = pets
-              }
-            }
+            this.setState({data: response.data})
+            // var petsMap = {}
+            // for(var i =0; i<response.data.length; i++){
+            //   var element = response.data[i]
+            //   if(petsMap.hasOwnProperty(element.gender)){
+            //     petsMap[element.gender].push(element.name)
+            //     petsMap[element.gender] = pets
+            //   }else{
+            //     var pets = new Array()
+            //     pets.push(element.name)
+            //     petsMap[element.gender] = pets
+            //   }
+            // }
 
-            var allHtml  = '<div class="list-group">';
-            var html = '';
-            for(element in petsMap){
-              html = '<a href="#" class="list-group-item list-group-item-action active" aria-current="true">'+ element +'</a>'
-              petsMap[element].forEach(petsElement => {
-                html += '<a href="#" class="list-group-item list-group-item-action">' + petsElement +'</a>'
-              });
-              allHtml += html;
-              console.log(html)
-            }
-            allHtml += '</div>'
-            this.setState = {allHtml: allHtml}
+            // this.setState = {petsMap:petsMap}
+
+            // var allHtml  = '<div class="list-group">';
+            // var html = '';
+            // for(element in petsMap){
+            //   html = '<a href="#" class="list-group-item list-group-item-action active" aria-current="true">'+ element +'</a>'
+            //   petsMap[element].forEach(petsElement => {
+            //     html += '<a href="#" class="list-group-item list-group-item-action">' + petsElement +'</a>'
+            //   });
+            //   allHtml += html;
+            //   console.log(html)
+            // }
+            // allHtml += '</div>'
+            // this.setState ( {allHtml: allHtml})
           }).catch(err => console.log(err))
     }
 
     render() {  
-      let html = this.state.allHtml;
+      let html =  this.state.data;
       console.log(html)
-      console.log("this.props")
       return (
         <form>
           <label>
