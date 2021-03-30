b34a438aa7e8d3dafea0ac132f46e449c6cb0c9b
zhijie.xu <zhijie.xu@openjawtech.com>
2021-03-30 12:25:27 +0800

    Pets web

--- src/component/weather.jsx
+++ src/component/weather.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import axios from 'axios';
 
 import '../config'
@@ -6,85 +6,53 @@ import '../config'
 class Weather extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {value: 'sydney',
-          city: '',
-          weather: '',
-          lastUpdateTime: '',
-          wind:'',
-          temperature: ''
-        };
-        this.handleChange = this.handleChange.bind(this);
     
-        let city = 'sydney'
-        this.setState({value: city})
-        let url = global.constants.serverUrl + city
+        let url = global.constants.serverUrl
         axios.get(url).then(
           (response) => {
-            this.setState({city: response.data.data.cityName,
-              temperature: response.data.data.temperature,
-              updateTime: response.data.data.updateTime,
-              weather: response.data.data.weather,
-              wind: response.data.data.wind,
-             });
-          })
+            var petsMap = {}
+            for(var i =0; i<response.data.length; i++){
+              var element = response.data[i]
+              if(petsMap.hasOwnProperty(element.gender)){
+                petsMap[element.gender].push(element.name)
+                petsMap[element.gender] = pets
+              }else{
+                var pets = new Array()
+                pets.push(element.name)
+                petsMap[element.gender] = pets
+              }
+            }
+
+            var allHtml  = '<div class="list-group">';
+            var html = '';
+            for(element in petsMap){
+              html = '<a href="#" class="list-group-item list-group-item-action active" aria-current="true">'+ element +'</a>'
+              petsMap[element].forEach(petsElement => {
+                html += '<a href="#" class="list-group-item list-group-item-action">' + petsElement +'</a>'
+              });
+              allHtml += html;
+              console.log(html)
+            }
+            allHtml += '</div>'
+            this.setState = {allHtml: allHtml}
+          }).catch(err => console.log(err))
     }
 
-    handleChange(event) {
-        let city = event.target.value;
-        this.setState({value: city})
-        let url = global.constants.serverUrl + city
-        axios.get(url).then(
-          (response) => {
-            this.setState({city: response.data.data.cityName,
-              temperature: response.data.data.temperature,
-              updateTime: response.data.data.updateTime,
-              weather: response.data.data.weather,
-              wind: response.data.data.wind,
-             });
-          })
-      }
-
-    render() {
-        return (
-          <form>
-            <label>
-              <select value={this.state.value} onChange={this.handleChange} class="form-control form-control-lg">
-                <option value="sydney">Sydney</option>
-                <option value="melbourne">Melbourne</option>
-                <option value="hobart">Hobart</option>
-              </select>
-            </label>
-    
-            <table class="table table-hover">
-              <tbody>
-                <tr>
-                  <td>City</td>
-                  <td>{this.state.city}</td>
-                </tr>          
-                
-                <tr>
-                  <td>Updated Time</td>
-                  <td>{this.state.updateTime}</td>
-                </tr>          
-                
-                <tr>
-                  <td>Weather</td>
-                  <td>{this.state.weather}</td>
-                </tr>          
-                
-                <tr>
-                  <td>Temperature</td>
-                  <td>{this.state.temperature}</td>
-                </tr>          
-                
-                <tr>
-                  <td>Wind</td>
-                  <td>{this.state.wind}</td>
-                </tr>
-              </tbody>
-            </table>
-          </form>
-        );
+    render() {  
+      let html = this.state.allHtml;
+      console.log(html)
+      console.log("this.props")
+      return (
+        <form>
+          <label>
+            <select class="form-control form-control-lg">
+              <option value="sydney">Sydney</option>
+              <option value="melbourne">Melbourne</option>
+              <option value="hobart">Hobart</option>
+            </select>
+          </label>
+        </form>
+      );
     }
 }
 
--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 global.constants = {
-    serverUrl:'http://120.26.45.1:8084/cityWeather/'
+    serverUrl:'http://5c92dbfae7b1a00014078e61.mockapi.io/owners'
 };
